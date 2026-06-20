f8fec52e6cb5d702a6078507417ed93d2c4a1d5c
dsmmarket307 <juanfelipemontanogarcia@gmail.com>
2026-06-20 11:01:51 -0500

    fix: agregar perfil al menu inferior

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -39,6 +39,15 @@ const tabs = [
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m8.66-13l-.87.5M4.21 17.5l-.87.5M20.66 17.5l-.87-.5M4.21 6.5l-.87-.5M21 12h-1M4 12H3" />
       </svg>
     )
+  },
+  {
+    href: "/perfil",
+    label: "Perfil",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+      </svg>
+    )
   }
 ]
 
