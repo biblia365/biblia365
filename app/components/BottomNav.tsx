dbbdbacc06fc03cf77f6f96acd20bf64d7a18f71
dsmmarket307 <juanfelipemontanogarcia@gmail.com>
2026-06-20 20:28:47 -0500

    feat: donaciones en menu inferior

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -32,11 +32,11 @@ const tabs = [
     )
   },
   {
-    href: "/frase-del-dia",
-    label: "Versiculo",
+    href: "/donaciones",
+    label: "Donar",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m8.66-13l-.87.5M4.21 17.5l-.87.5M20.66 17.5l-.87-.5M4.21 6.5l-.87-.5M21 12h-1M4 12H3" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
     )
   },
